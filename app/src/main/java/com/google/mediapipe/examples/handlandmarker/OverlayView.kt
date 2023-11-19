bc0f5959cbb4d762fe22f170385895574ef56052
Baik Seo Woon <sunnyb@hku.hk>
2023-11-19 20:23:04 +0800

    removed copyright lol

--- app/src/main/java/com/google/mediapipe/examples/handlandmarker/OverlayView.kt
+++ app/src/main/java/com/google/mediapipe/examples/handlandmarker/OverlayView.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright 2022 The TensorFlow Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *             http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.google.mediapipe.examples.handlandmarker
 
 import ai.onnxruntime.OnnxTensor
@@ -25,7 +10,6 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.mediapipe.tasks.vision.core.RunningMode
 import com.google.mediapipe.tasks.vision.handlandmarker.HandLandmarker
@@ -36,6 +20,7 @@ import kotlin.math.min
 
 class OverlayView(context: Context?, attrs: AttributeSet?) :
     View(context, attrs) {
+//    var textView: TextView = findViewById(R.id.textView)
     private fun createORTSession( ortEnvironment: OrtEnvironment ) : OrtSession {
         val modelBytes = resources.openRawResource( R.raw.sklearn_model ).readBytes()
         return ortEnvironment.createSession( modelBytes )
@@ -115,7 +100,8 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
                         pointPaint
                     )
                 }
-                runPrediction(hand_coordinates,ortSession,ortEnvironment)
+                var prediction = runPrediction(hand_coordinates,ortSession,ortEnvironment)
+                Log.d("Logging",prediction)
                 HandLandmarker.HAND_CONNECTIONS.forEach {
                     canvas.drawLine(
                         handLandmarkerResult.landmarks().get(0).get(it!!.start())
